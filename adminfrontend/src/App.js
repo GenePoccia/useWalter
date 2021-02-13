29f898f091b17d4eb2899f4d551f1facc7e61453
Eugenio <pocciaeugenio@gmail.com>
2021-02-12 22:29:31 -0500

    admin front end

---
+++ adminfrontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header, Segment } from "semantic-ui-react";
+
+const HeaderExampleTextAlignment = () => (
+  <Segment>
+    <Header as="h3" textAlign="center">
+      Walter Apartments
+    </Header>
+  </Segment>
+);
+
+export default HeaderExampleTextAlignment;
